73c1658925bd0f1cf54230680710ccebe327dc37
liumaowen <liumaowen@wiskind.cn>
2026-05-06 18:11:25 +0800

    1

--- src/main/java/com/example/qylbackend/model/Order.java
+++ src/main/java/com/example/qylbackend/model/Order.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import java.time.LocalDateTime;
 
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
  */
 @Entity
 @Data
+@Table(name = "\"order\"")
 public class Order {
 
     @Id
